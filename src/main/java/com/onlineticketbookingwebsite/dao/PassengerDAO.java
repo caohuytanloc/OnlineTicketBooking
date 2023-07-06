4b1bc3249a527d5e0b69534059e359623c6a7988
LocNlu <18130128@st.hcmuaf.edu.vn>
2023-07-07 00:46:03 +0700

    Update PassengerDAO.java

--- src/main/java/com/onlineticketbookingwebsite/dao/PassengerDAO.java
+++ src/main/java/com/onlineticketbookingwebsite/dao/PassengerDAO.java
@@ -8,7 +8,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.sql.Date;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 public class PassengerDao {
     private static PassengerDao instance;
@@ -70,7 +74,7 @@ public class PassengerDao {
             ps.setString(3, phoneNumber);
             ps.setObject(4, birthday);
             ps.setString(5, gender);
-            ps.setString(6, address);
+            ps.setString(6,  address);
             ps.setString(7, identification);
             ps.setString(8, email);
 
@@ -86,33 +90,44 @@ public class PassengerDao {
         }
         return result;
     }
-
     public String getId() {
-        String result = "";
+        String result="";
 
 
-        String query = "SELECT id\n" +
-                "FROM passengers\n" +
-                "ORDER BY id DESC\n" +
-                "LIMIT 1;";
-        try (PreparedStatement ps = DBConnect.getInstance().get(query)) {
+            String query = "SELECT id\n" +
+                    "FROM passengers\n" +
+                    "ORDER BY id DESC\n" +
+                    "LIMIT 1;";
+            try (PreparedStatement ps = DBConnect.getInstance().get(query)) {
 
-            try (ResultSet resultSet = ps.executeQuery()) {
-                if (resultSet.next()) {
-                    result = resultSet.getString(1);
+                try (ResultSet resultSet = ps.executeQuery()) {
+                    if (resultSet.next()) {
+                        result = resultSet.getString(1);
 
+                    }
                 }
+            } catch (SQLException | ClassNotFoundException e) {
+                e.printStackTrace();
             }
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
 
         return result;
 
     }
 
     public static void main(String[] args) {
+//        LocalDate date = LocalDate.of(2023, 7, 6); // Khai báo ngày: 2023-07-06
+//        LocalTime time = LocalTime.of(15, 30, 0); // Khai báo thời gian: 15:30:00
+//
+//        LocalDateTime dateTime = LocalDateTime.of(date, time);
+//        System.out.println(new PassengerDao().createPassenger("P006","Thắm","Huỳnh",LocalDateTime.now(),"12334","03939","h1@gmial.com","diachi","Nữ"));
+//        String id_passenger = String.valueOf(((Integer.parseInt(new PassengerDao().getId().substring(3)) + 1)));
+     String birth = "2023-07-09";
+        LocalDate date = LocalDate.parse(birth);
 
+        LocalDateTime dateTime = date.atStartOfDay();
+
+        System.out.println("LocalDateTime: " + dateTime);
     }
+
 }
 
